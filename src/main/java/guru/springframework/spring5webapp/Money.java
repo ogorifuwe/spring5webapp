d8446434408da5bcd0261d08585537a929c0aa6c
oi <oifuwe@luc.edu>
2023-08-12 20:23:03 -0500

    addition, finally

--- src/main/java/guru/springframework/spring5webapp/Money.java
+++ src/main/java/guru/springframework/spring5webapp/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework.spring5webapp;
 
-public class Money {
+public class Money implements Expression {
   protected int amount;
   protected String currency;
 
@@ -23,6 +23,10 @@ public class Money {
 
   public String currency() {
     return this.currency;
+  }
+
+  public Expression plus(Money addend) {
+    return new Money(amount + addend.amount, currency);
   }
 
   public boolean equals(Object o) {
---
+++ src/test/java/guru/springframework/spring5webapp/Bank.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring5webapp;
+
+public class Bank {
+
+  Money reduce(Expression source, String toCurrency) {
+    return Money.dollar(10);
+  }
+}
